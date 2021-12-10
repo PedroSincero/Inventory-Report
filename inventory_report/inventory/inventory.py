81de7eeb65b502d9ee4b4b75b44cdcb1f909f54b
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-12-10 19:31:23 -0300

    bug - adicionando req 3 - [pending]

---
+++ inventory_report/inventory/inventory.py
@@ -0,0 +1,23 @@
+from inventory_report.reports.simple_report import SimpleReport
+import csv
+
+# inventory_report/reports/simple_report.py
+# exec("../reports/simple_report.py")
+# from ..reports.complete_report import CompleteReport
+# ("/home/el/foo2/mylib.py")
+
+class Inventory:
+    @classmethod
+    def import_data(cls, path, type_report):
+        jobs = []
+        with open(path) as file:
+            jobs_reade = csv.DictReader(file)
+            for job in jobs_reade:
+                jobs.append(job)
+            # if type_report == "completo":
+                # return CompleteReport.generate(jobs)
+            if type_report == "simples":
+                return SimpleReport.generate(jobs)
+
+
+print(Inventory.import_data('inventory_report/data/inventory.csv', 'completo'))
