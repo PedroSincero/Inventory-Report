c9a6ce271a8a2bd13d2c12ca927981d272d4e35f
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-12-13 15:20:27 -0300

    fix - req 4,5,6

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,22 +1,38 @@
 from inventory_report.reports.simple_report import SimpleReport
+import json
 import csv
-
-# inventory_report/reports/simple_report.py
-# exec("../reports/simple_report.py")
+import xml.etree.ElementTree as ET
 from inventory_report.reports.complete_report import CompleteReport
-
-# ("/home/el/foo2/mylib.py")
 
 
 class Inventory:
     @classmethod
     def import_data(cls, path, type_report):
         jobs = []
+        type = {'completo': CompleteReport, 'simples': SimpleReport}
         with open(path) as file:
-            jobs_reade = csv.DictReader(file)
+            if path.endswith(".csv"):
+                jobs_reade = csv.DictReader(file)
+            if path.endswith(".json"):
+                jobs_reade = json.load(file)
+            if path.endswith(".xml"):
+                jobs_reade = read_xml(path)
             for job in jobs_reade:
                 jobs.append(job)
-            if type_report == "completo":
-                return CompleteReport.generate(jobs)
-            if type_report == "simples":
-                return SimpleReport.generate(jobs)
+            return type[type_report].generate(jobs)
+
+
+def read_xml(path):
+    tree = ET.parse(path)
+    root = tree.getroot()
+    jobs_reade = []
+    for record in root:
+        result_dict = {}
+        for child_record in record:
+            result_dict[child_record.tag] = child_record.text
+        jobs_reade.append(result_dict)
+    return jobs_reade
+
+
+# Agradecimentos A
+# Lucas Martins, Ederson Rodrigues, Gabriel Essenio - Turma 10 - Tribo B
